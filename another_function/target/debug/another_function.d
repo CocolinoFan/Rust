1043d744f49b51322811b021100e2a13f4a70bdd
CocolinoFan <arvamircea@gmail.com>
2024-06-13 14:27:10 +0100

    Re-install OS

---
+++ another_function/target/debug/another_function.d
@@ -0,0 +1,1 @@
+/home/cocolino/Work/Rust/another_function/target/debug/another_function: /home/cocolino/Work/Rust/another_function/src/main.rs
---
+++ another_function/target/debug/deps/another_function-c426586fa87a84e1.d
@@ -0,0 +1,5 @@
+/home/cocolino/Work/Rust/another_function/target/debug/deps/another_function-c426586fa87a84e1: src/main.rs
+
+/home/cocolino/Work/Rust/another_function/target/debug/deps/another_function-c426586fa87a84e1.d: src/main.rs
+
+src/main.rs:
